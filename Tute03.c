6b921e3fd5124b9cd232e0a19513d91f9353e8a6
Methmi Himaya Pathirana <149407589+MethmiHP@users.noreply.github.com>
2023-12-13 17:46:52 +0530

    Update Tute03.c

--- Tute03.c
+++ Tute03.c
@@ -14,18 +14,22 @@ sum = 1+2+3+...+10 = 55 */
 #include <stdio.h>
 int main() {
 
+  //variable declaration
   int number, count, sum;
   count = 1;
   sum = 0;
-  
+
+  //input
   printf("Enter upto which number that you have to get the sum : ");
   scanf("%d",&number);
 
+  //while loop
   while(count <= number)
     {
-      sum = sum + count;
+      sum = sum + count; //calculate the sum
       count++ ;
     }
+  //output
   printf("Sum of the numbers : %d",sum);
   
   return 0;
