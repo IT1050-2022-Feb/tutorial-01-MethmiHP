00955addf70d4f521dcff01308ceba845da59fff
Methmi Himaya Pathirana <149407589+MethmiHP@users.noreply.github.com>
2023-12-13 08:29:33 +0530

    Update Tute02.c

--- Tute02.c
+++ Tute02.c
@@ -20,5 +20,28 @@ Amount = 30 x 50 + (50-30) x 40 = 2300*/
 
 int main() {
   
+  //variable declaration
+  float distance, amount;
+
+  //inputs
+  printf("Enter the distance the van has travelled : ");
+  scanf("%f",&distance);
+
+  //if condition
+  if(distance < 0)
+  {
+  		printf("There is an error");
+  }
+  else if(distance <= 30)
+	{
+  		amount = distance * 50.00; //calculation the amount to be paid
+  		printf("The amount to be paid is : %.2f",amount); //output
+	}
+ else if(distance > 30)
+	{
+  		amount = (30 * 50) + ((distance - 30) * 40);
+  		printf("The amount to be paid is : %.2f",amount); //output
+	}
+  
   return 0;
 }
